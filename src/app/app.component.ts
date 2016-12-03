8f26da026b174fe23f8bedbe5d6501a2a4dbb4df
Dominic Muttel <dominic.muttel@gmail.com>
2016-12-03 15:39:24 -0700

    recipe-list in wrong place

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { HeaderComponent } from './header.component';
+import { RecipesComponent } from './recipes';
+
 @Component({
   selector: 'rb-root',
   templateUrl: './app.component.html'
