e142dbe697f59e966aaecb80c24dce15ea68215a
Dominic Muttel <dominic.muttel@gmail.com>
2016-12-04 10:00:14 -0700

    recipe lit and item constructor created / recipe model created - but not working

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe }  from '../recipe';
+// import { RecipeItemComponent } from './recipe-item.component';
+
 @Component({
   selector: 'rb-recipe-list',
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-
+    recipes: Recipe[] = [];
+    recipe = new Recipe('Dummy', 'Dummy', 'http://guides.global/images/guides/global/dummy_web_page.jpg');
   constructor() { }
 
   ngOnInit() {
